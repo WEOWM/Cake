c18e6b7e742e1b5a81e7b4de5cb579be927b3338
basim <basims0077@gmail.com>
2022-10-27 16:03:01 +0530

    upadate

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,16 +3,48 @@ import './Footer.css'
 
 const Footer = () => {
   return (
-    <div>
-        <div className="footer">
-            <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
+    <div >
+      <div className="container-fluid ">
+        <div className="row footer text-right ">
+
+          <div className="fd col-md-3 lg-4 mt-5">
+            <li>About Us</li>
+            <li>Careers</li>
+            <li>Corporate Order</li>
+            <li>Frachise Enquiry</li>
+          </div>
+
+
+          <div className="fd col-md-3 lg-4 mt-5">
+            <li>FAQ</li>
+            <li>Contact US</li>
+            <li>Cunstomer Reviews</li>
+            <li>affiliate program</li>
+            <li>Wishes</li>
+          </div>
+          <div className="fd col-md-3 lg-4  mt-5 ">
+            <li>Blog-Celebate Relations</li>
+            <li>Bug Bounty</li>
+            <li>Sitemap</li>
+            <li>Data Security</li>
+            <li>in News</li>
+          </div>
+          <div className="fd col-md-3 lg-4  mt-5">
+            <li>Refund Policy</li>
+            <li>Cancellation Policy</li>
+            <li>Privacy Policy</li>
+            <li>Payments And Secturity</li>
+            <li>Terms And Conditions</li>
+            
+          </div>
+
+
+
         </div>
+
+
+
+      </div>
     </div>
   )
 }
